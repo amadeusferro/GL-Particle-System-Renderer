7de84de5ce1224af90dd4097f934b92e4fabf09a
Amadeus Ferro <amadeus.ferro@gmail.com>
2025-03-01 21:52:36 -0300

    fast compiling bash script added

---
+++ build_and_run.sh
@@ -0,0 +1,19 @@
+make clean
+
+make
+
+if [ $? -eq 0 ]; then
+    ./bin/myProgram
+
+    EXIT_CODE=$?
+    
+    make clean
+
+    if [ $EXIT_CODE -eq 0 ]; then
+        echo "Program executed successfully."
+    else
+        echo "Program failed with exit code $EXIT_CODE."
+    fi
+else
+    echo "Compiling failed. Check for errors."
+fi
